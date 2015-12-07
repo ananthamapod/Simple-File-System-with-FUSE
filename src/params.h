43cfe85231831febc64112dfc675886f30f52b30
Kartik Chopra <Kartik@Kartiks-MBP.home>
2015-12-06 22:30:48 -0500

    Params.h first init

--- src/params.h
+++ src/params.h
@@ -23,10 +23,72 @@
 // maintain bbfs state in here
 #include <limits.h>
 #include <stdio.h>
+#include <list.h>
+
+
+#include "params.h"
+#include "block.h"
+
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <fuse.h>
+#include <libgen.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+#include "log.h"
+
+struct inode {
+        struct list_head        i_list;              //list of i-nodes
+        struct list_head        i_data_entry;            //list of data entries [essentially the whole array]
+        int   		            i_inode;              // i-node number
+        uid_t                   i_uid;                // user_id of owner process - might need these for later
+        gid_t                   i_gid;                // group_id of ownder process - might need these for later
+        unsigned long           i_file_zize_in_blocks;  //file size in blocks
+        
+        struct super_block      *i_sb;               //which superblock it points to
+        atomic_t                i_writecount;         // count of writes might need these for later
+       
+};
+
+struct inode_bitmap{
+	int bitmap[1024];
+};
+struct data_bitmap{
+	int bitmap[1024];
+};
+
+struct inodes_table {
+	struct inode table[1024]; //How big should the table be guys? 
+}; 
+
+//This is the first block of the filysystem (considering a file system is just a really long array..which it actually is in memory)
+struct super_block{
+	int 						total_i_nodes;
+	int 						total_data_entries; // total data entries 
+	unsigned long 				sb_blocksize 		// in bytes
+	struct  super_operations 	s_op;  				/* superblock methods */
+};
+
+// Linux kernel super block operations from the original kernel 
+struct super_operations {
+        struct inode *(*alloc_inode) (struct super_block *sb);
+        void (*destroy_inode) (struct inode *);
+        void (*read_inode) (struct inode *);
+        void (*write_inode) (struct inode *, int);
+        void (*delete_inode) (struct inode *);
+        int (*sync_fs) (struct super_block *, int);
+};
+
 struct sfs_state {
     FILE *logfile;
     char *diskfile;
-};
+
 #define SFS_DATA ((struct sfs_state *) fuse_get_context()->private_data)
 
 #endif
